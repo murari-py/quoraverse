fdde97140801b9ea11e53925f02c3e99b2445a8d
murari-py <murarissdjango@gmail.com>
2025-04-11 18:07:56 +0530

    Add user sign-up and login features

---
+++ users_management/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from users_management import views
+
+
+
+urlpatterns = [
+    path("home/", views.home, name="home"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path(
+        "",
+        auth_views.LoginView.as_view(template_name="users_management/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+]
---
+++ users_management/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+
+
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
+@login_required
+def home(request):
+    return render(request, 'users_management/home.html')
+
+
+
+class SignUpView(FormView):
+    template_name = 'users_management/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
